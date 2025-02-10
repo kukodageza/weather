a01083f7094b63674332fc805d7acdf1b6cf4e24
Geza Kukoda <geza.kukoda@partner.bmw.de>
2025-02-10 12:00:51 +0100

    port

--- port-forward.ps1
+++ port-forward.ps1
@@ -1,28 +1,14 @@
-# Function to stop processes using specific ports
-function Stop-PortProcess {
-    param (
-        [int]$Port
-    )
-    $processes = netstat -ano | Select-String ":$Port" | ForEach-Object {
-        $_.ToString().Split(" ", [System.StringSplitOptions]::RemoveEmptyEntries)[-1]
-    }
-    foreach ($processId in $processes) {
-        try {
-            Stop-Process -Id $processId -Force -ErrorAction Stop
-            Write-Host "Stopped process with PID $processId using port $Port"
-        } catch {
-            Write-Host "Failed to stop process with PID $processId using port $Port"
-        }
-    }
+# Start port-forwarding for Prometheus and Grafana
+Write-Host "Starting port-forwarding for Prometheus and Grafana..."
+
+# Forward Grafana port
+Start-Job -ScriptBlock {
+    kubectl port-forward svc/prometheus-kube-prometheus-prometheus 9090:9090 --namespace monitoring
 }
 
-# Stop processes using ports 3000 and 9090
-Stop-PortProcess -Port 3000
-Stop-PortProcess -Port 9090
+# Forward Prometheus port
+Start-Job -ScriptBlock {
+    kubectl port-forward svc/prometheus-grafana 3000:80 --namespace monitoring
+}
 
-# Wait a moment to ensure processes are fully stopped
-Start-Sleep -Seconds 20
-
-# Start port-forwarding for Prometheus and Grafana
-kubectl port-forward svc/prometheus 9090:9090 &
-kubectl port-forward svc/grafana 3000:3000 &
+Write-Host "Port-forwarding started."
